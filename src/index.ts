c139b8cb59ada3a9026c6442d959ab672fb4509a
Andrea Lin <andrea.lin3110@gmail.com>
2022-08-03 22:09:26 +0200

    📈 v1.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,29 @@
-type Person = {
-  name: string
-  fam: string
+import { parse, HTMLElement } from "node-html-parser"
+
+export interface Metadata {
+  [key: string]: string
 }
 
-export const helloWorld = (person: Person): void => {
-  console.log(`Hello ${person.name} ${person.fam}`)
+const getPropertyContent = (metaElement: HTMLElement, name: string): string | null => {
+  const propertyName = metaElement.getAttribute("property") || metaElement.getAttribute("name")
+  return propertyName == name ? metaElement.getAttribute("content") || "" : null
+}
+
+export const parseMetadata = (html: string, properties: string[]): Metadata => {
+  const $ = parse(html)
+
+  const metas = $.querySelectorAll("meta")
+
+  const metadatas: Metadata = {}
+
+  metas.forEach((meta) => {
+    properties.forEach((prop) => {
+      const content = getPropertyContent(meta, prop)
+      if (content) {
+        metadatas[prop] = content
+      }
+    })
+  })
+
+  return metadatas
 }
